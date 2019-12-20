6cb1b9a220f53e618c8fc74521b8b6a14fcee214
Noel Yunginger <noelman@noels-mbp.lan>
2019-12-19 21:35:29 -0800

    connected to the ibc database

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ import os
 
 #init
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+#Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/ibc'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
 
 @app.route('/', methods=['GET'])
 def getStuff():
-    return jsonify({'msg' : 'waddup'})
+    return jsonify({'msg' : 'waddup!!!!'})
 
 #run server
 if __name__ == '__main__':
